6abff7d4465f0b6fce984343cf835ecf1b23fed7
jjfrost <jfrost@microsoft.com>
2025-10-14 21:54:04 -0700

    First commit and code update to use Agent Framework

---
+++ Scripts/load_age.ps1
@@ -0,0 +1,12 @@
+$resourceGroupName = "jfrost-devlab"
+
+$postgresServerName = az postgres flexible-server list --resource-group $resourceGroupName --query "[0].name" --output tsv
+
+
+$postgresServerName = "jfrost-pg004"
+
+az postgres flexible-server parameter set --resource-group $resourceGroupName --server-name $postgresServerName --name azure.extensions --value azure_ai,pg_diskann,vector,age,azure_storage
+
+az postgres flexible-server parameter set --resource-group $resourceGroupName --server-name $postgresServerName --name shared_preload_libraries --value age,azure_storage,pg_cron,pg_stat_statements
+
+az postgres flexible-server restart --resource-group $resourceGroupName --name $postgresServerName
